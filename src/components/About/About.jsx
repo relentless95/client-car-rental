6fe41911d7db8716d68ab0e7351e2b2f33db3ec7
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-05-27 12:11:43 +0200

    about section done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ import car from "../../../public/images/about/parked_cars.jpg";
 function About() {
   return (
     <>
-      <div>About</div>
+      {/* <div>About</div> */}
       <section className="about-section">
         <div className="container">
           <div className="about-main">
@@ -15,14 +15,15 @@ function About() {
             </div>
 
             <div className="about-main__text">
-              <h3>About Us</h3>
+              <h3>About us</h3>
+             
               <h2>More than 150+ special collection cars</h2>
               <p>
                 Get the car of your dreams with the installments of your choice.
                 There are various attractive offers through our collaboration
-                with various trusted leasing partners.
+                with various trusted leasing partners. Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit aperiam quis vero, illo tempora aut accusantium obcaecati, quasi fuga error autem optio reiciendis, eius tenetur quam saepe tempore quisquam!
               </p>
-              <button className="about-button__explore">
+              <button className="about-main__text-button">
                 See all our cars
               </button>
             </div>
