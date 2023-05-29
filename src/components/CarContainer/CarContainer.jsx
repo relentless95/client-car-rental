ed2a7c362b45fc9a03453c6564340be62fa5e3d9
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-05-29 02:35:54 +0200

    working on carContainer

---
+++ src/components/CarContainer/CarContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CarContainer({ data, carID }) {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default CarContainer;
--- src/components/SelectCar/SelectCar.jsx
+++ src/components/SelectCar/SelectCar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./SelectCar.scss";
 import { carData } from "./carData";
+import CarContainer from "../CarContainer/CarContainer";
 
 function SelectCar() {
   const [active, setActive] = useState("FirstCar");
@@ -23,7 +24,7 @@ function SelectCar() {
               <h3>Vehicle Models</h3>
               <h2>Our rental fleet</h2>
               <p>
-                Choose from a varitey of our amazing vehicles to rent for your
+                Choose from a variety of our amazing vehicles to rent for your
                 next adventure or business trip
               </p>
             </div>
@@ -33,12 +34,69 @@ function SelectCar() {
                 <button
                   className={`${coloringButton("btn1")}`}
                   onClick={() => {
-                    setActive("SecondCar");
+                    setActive("FirstCar");
                     btnID("btn1");
                   }}
-                />
-                
+                >
+                  Ford Mustang
+                </button>
+
+                <button
+                  className={`${coloringButton("btn2")}`}
+                  onClick={() => {
+                    setActive("SecondCar");
+                    btnID("btn2");
+                  }}
+                >
+                  Honda Accord
+                </button>
+
+                <button
+                  className={`${coloringButton("btn3")}`}
+                  onClick={() => {
+                    setActive("ThirdCar");
+                    btnID("btn3");
+                  }}
+                >
+                  Toyota Corolla
+                </button>
+
+                <button
+                  className={`${coloringButton("btn4")}`}
+                  onClick={() => {
+                    setActive("FourthCar");
+                    btnID("btn4");
+                  }}
+                >
+                  VolksWagen Golf
+                </button>
+
+                <button
+                  className={`${coloringButton("btn5")}`}
+                  onClick={() => {
+                    setActive("FifthCar");
+                    btnID("btn5");
+                  }}
+                >
+                  VolksWagen Tiguan
+                </button>
               </div>
+
+              {active === "FirstCar" && (
+                <CarContainer data={carData} carID={0} />
+              )}
+              {active === "SecondCar" && (
+                <CarContainer data={carData} carID={1} />
+              )}
+              {active === "ThirdCar" && (
+                <CarContainer data={carData} carID={2} />
+              )}
+              {active === "FourthCar" && (
+                <CarContainer data={carData} carID={3} />
+              )}
+              {active === "FifthCar" && (
+                <CarContainer data={carData} carID={4} />
+              )}
             </div>
           </div>
         </div>
