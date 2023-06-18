541f6fe7462202830087c814168d3829cc7915bf
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-06-19 00:46:13 +0200

    working o login page css

--- src/pages/Login/LogIn.jsx
+++ src/pages/Login/LogIn.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
+// import React from 'react'
+import BannerPages from "../../components/bannerPages/BannerPages";
+import "./Login.scss";
 
 function LogIn() {
   return (
     <>
-     <div>LogIn</div>
-     <form>
-      <input/>
-      
-     </form>
+      <section className="login-page">
+        <div className="container">
+          <div className="login-container">
+          <div className="login-container-inner">
+            <form action="#" className="login-form">
+              <div className="login-form__title">
+                <h2>Log in</h2>
+              </div>
+              <div className="login-form_group">
+                <input
+                  type="text"
+                  className="form__input"
+                  placeholder="Username"
+                  id="username"
+                  required
+                />
+                <label htmlFor="username" className="form__label">
+                  User name
+                </label>
+              </div>
+
+              <div className="login-form_group">
+                <input
+                  type="text"
+                  className="form__input"
+                  placeholder="Password"
+                  id="password"
+                  required
+                />
+                <label htmlFor="password" className="form__label">
+                  Password
+                </label>
+              </div>
+
+              <div className="login-form-group">
+                <button className="btn">log in</button>
+              </div>
+            </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* <BannerPages/> */}
     </>
-   
-  )
+  );
 }
 
-export default LogIn
+export default LogIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,8 +2,62 @@ import React from 'react'
 
 function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <section className="login-page">
+        <div className="container">
+          <div className="login-container">
+            <form action="#" className="login-form">
+              <div className="login-form_username">
+                <h2>Log in</h2>
+              </div>
+              <div className="login-form_group">
+                <input
+                  type="text"
+                  className="form__input"
+                  placeholder="Username"
+                  id="username"
+                  required
+                />
+                <label htmlFor="username" className="form__label">
+                  Full name
+                </label>
+              </div>
+
+              <div className="login-form_group">
+                <input
+                  type="email"
+                  className="form__input"
+                  placeholder="Email address"
+                  id="email"
+                  required
+                />
+                <label htmlFor="email" className="form__label">
+                  Email address
+                </label>
+              </div>
+
+              <div className="login-form_group">
+                <input
+                  type="text"
+                  className="form__input"
+                  placeholder="Password"
+                  id="password"
+                  required
+                />
+                <label htmlFor="password" className="form__label">
+                  Password
+                </label>
+              </div>
+
+              <div className="login-form-group">
+                <button className="btn">log in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default SignUp
