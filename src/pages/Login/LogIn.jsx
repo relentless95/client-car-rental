7626db0efdce4f2167d43f0411b951948fb31fda
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-06-28 00:34:46 +0200

    adding the authentication logic in the context

--- src/pages/Login/LogIn.jsx
+++ src/pages/Login/LogIn.jsx
@@ -1,24 +1,62 @@
 // import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.scss";
 import { useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../../context/auth.context";
 
 function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
 
+  const API_URL = "http://localhost:5005";
+
+  const navigate = useNavigate();
+
+  // const {storeToken, authenticateUser} = useContext(AuthContext);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+
+    axios
+      .post(`${API_URL}/auth/login`, requestBody)
+      .then((response) => {
+        console.log("JWT token", response.data.authToken);
+
+        navigate("/profile");
+
+        // storeToken(response.data.authToken);
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
 
   return (
     <>
       <section className="login-page">
         {/* <div className="container"> */}
-          <div className="login-container">
+        <div className="login-container">
           <div className="login-container-inner">
-            <form action="#" className="login-form">
+            <form onSubmit={handleLoginSubmit} className="login-form">
               <div className="login-form__title">
                 <h2>Login</h2>
               </div>
               <div className="login-form_group">
                 <input
+                  value={email}
+                  onChange={handleEmail}
                   type="text"
                   className="login-form__input"
                   placeholder="Email"
@@ -32,6 +70,8 @@ function LogIn() {
 
               <div className="login-form_group">
                 <input
+                  value={password}
+                  onChange={handlePassword}
                   type="text"
                   className="login-form__input"
                   placeholder="Password"
@@ -44,24 +84,24 @@ function LogIn() {
               </div>
 
               <div className="login-form-group">
-                <button className="btn">Login</button>
+                <button type="submit" className="btn">
+                  Login
+                </button>
               </div>
             </form>
             <div className="login-form_login-link">
-                {errorMessage && (
-                  <p className="error-message-universal">{errorMessage}</p>
-                )}
-                <p>Don't have an account?</p>
-                <button className="btn-black">
-                  <Link to={"/signup"}> Sign up</Link>
-                </button>
-              </div>
-            
+              {errorMessage && (
+                <p className="error-message-universal">{errorMessage}</p>
+              )}
+              <p>Don't have an account?</p>
+              <button className="btn-black">
+                <Link to={"/signup"}> Sign up</Link>
+              </button>
             </div>
           </div>
+        </div>
         {/* </div> */}
       </section>
-  
     </>
   );
 }
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./SignUp.scss";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 function SignUp() {
   const [email, setEmail] = useState("");
@@ -14,82 +17,98 @@ function SignUp() {
   const handleUsername = (e) => setUsername(e.target.value);
   const handlePassword = (e) => setPassword(e.target.value);
 
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password, username };
+
+    axios
+      .post(`${API_URL}/auth/signup`, requestBody)
+      .then((response) => {
+        // console.log("here response---->", response)
+        navigate("/login");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
   return (
     <>
       <section className="signup-page">
         {/* <div className="container"> */}
-          <div className="signup-container">
-            <div className="signup-container-inner">
-              <form action="#" className="signup-form">
-                <div className="signup-form__title">
-                  <h2>sign up</h2>
-                </div>
+        <div className="signup-container">
+          <div className="signup-container-inner">
+            <form onSubmit={handleSignupSubmit} className="signup-form">
+              <div className="signup-form__title">
+                <h2>sign up</h2>
+              </div>
 
-                <div className="signup-form_group">
-                  <input
-                    value={email}
-                    onChange={handleEmail}
-                    type="email"
-                    className="signup-form__input"
-                    placeholder="Email"
-                    id="email"
-                    name="email"
-                    required
-                  />
-                  <label htmlFor="email" className="signup-form__label">
-                    Email
-                  </label>
-                </div>
-                <div className="signup-form_group">
-                  <input
-                    value={username}
-                    onChange={handleUsername}
-                    type="text"
-                    className="signup-form__input"
-                    placeholder="Username"
-                    id="username"
-                    name="username"
-                    required
-                  />
-                  <label htmlFor="username" className="signup-form__label">
-                    Username
-                  </label>
-                </div>
+              <div className="signup-form_group">
+                <input
+                  value={email}
+                  onChange={handleEmail}
+                  type="email"
+                  className="signup-form__input"
+                  placeholder="Email"
+                  id="email"
+                  name="email"
+                  required
+                />
+                <label htmlFor="email" className="signup-form__label">
+                  Email
+                </label>
+              </div>
+              <div className="signup-form_group">
+                <input
+                  value={username}
+                  onChange={handleUsername}
+                  type="text"
+                  className="signup-form__input"
+                  placeholder="Username"
+                  id="username"
+                  name="username"
+                  required
+                />
+                <label htmlFor="username" className="signup-form__label">
+                  Username
+                </label>
+              </div>
 
-                <div className="signup-form_group">
-                  <input
-                    value={password}
-                    onChange={handlePassword}
-                    type="text"
-                    className="signup-form__input"
-                    placeholder="Password"
-                    id="password"
-                    name="password"
-                    required
-                  />
-                  <label htmlFor="password" className="signup-form__label">
-                    Password
-                  </label>
-                </div>
+              <div className="signup-form_group">
+                <input
+                  value={password}
+                  onChange={handlePassword}
+                  type="text"
+                  className="signup-form__input"
+                  placeholder="Password"
+                  id="password"
+                  name="password"
+                  required
+                />
+                <label htmlFor="password" className="signup-form__label">
+                  Password
+                </label>
+              </div>
 
-                <div className="signup-form-group">
-                  <button className="btn" type="submit">
-                    Sign up
-                  </button>
-                </div>
-              </form>
-
-              <div className="signup-form_signup-link">
-                {errorMessage && (
-                  <p className="error-message-universal">{errorMessage}</p>
-                )}
-                <p>Already have an account?</p>
-                <button className="btn-black">
-                  <Link to={"/login"}> Login</Link>
+              <div className="signup-form-group">
+                <button className="btn" type="submit">
+                  Sign up
                 </button>
               </div>
+            </form>
+
+            <div className="signup-form_signup-link">
+              {errorMessage && (
+                <p className="error-message-universal">{errorMessage}</p>
+              )}
+              <p>Already have an account?</p>
+              <button className="btn-black">
+                <Link to={"/login"}> Login</Link>
+              </button>
             </div>
           </div>
+        </div>
         {/* </div> */}
       </section>
     </>
